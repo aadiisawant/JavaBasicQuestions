fb86267ccff0332e80aea2fff74459fc1a2b2961
aadiisawant <adisawant0178@gmail.com>
2024-02-12 15:42:31 +0530

    Ques Added

--- JavaQues/src/main/java/FindArrayTriplets.java
+++ JavaQues/src/main/java/FindArrayTriplets.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class FindArrayTriplets {
+import java.util.HashSet;
+
+//How To Find Array Triplets With Sum Of Two Elements Equals Third?
+public class FindArrayTriplets {
+
+    public static void main(String[] args) {
+        int arr [] ={21, 13, 47, 61, 34, 40, 55, 71, 87};
+        HashSet<Integer> hashSet = new HashSet<>();
+        for(int i=0;i<arr.length;i++){
+            hashSet.add(arr[i]);
+        }
+
+        System.out.println(hashSet);
+        for(int i=0;i<arr.length;i++){
+            for(int j= i+1; j< arr.length;j++){
+                if(hashSet.contains(arr[i]+arr[j])){
+                    System.out.println("[ "+arr[i]+", "+arr[j]+", "+(arr[i]+arr[j])+" ]");
+                }
+            }
+        }
+
+    }
 }
