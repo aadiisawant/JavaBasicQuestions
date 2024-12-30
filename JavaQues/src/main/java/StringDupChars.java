f862804d37850350bbb083bbb2d623d51bfafab7
aadiisawant <adisawant0178@gmail.com>
2024-12-30 19:42:21 +0530

    StringDuplicateChars

---
+++ JavaQues/src/main/java/StringDupChars.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class StringDupChars {
+    public static void main(String args[]){
+        String words = "Better Butter";
+        char [] carr = words.toCharArray();
+        HashMap<Character, Integer> hmap = new HashMap<>();
+        for(char c : carr){
+            if(hmap.containsKey(c)){
+                hmap.put(c,hmap.get(c)+1);
+            }
+            else{
+                hmap.put(c, 1);
+            }
+        }
+        Set<Character> charKeys = hmap.keySet();
+        for(Character c : charKeys){
+            if(hmap.get(c) >1) System.out.println(c+" : "+hmap.get(c));
+        }
+
+    }
+}
